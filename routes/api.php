080456dce2f56a73b2ddc96afd65278937e8a469
seeda06 <86487372+seeda06@users.noreply.github.com>
2022-02-03 17:37:58 +0700

    week11

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\LocationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('book', BookController::class);
+Route::apiResource('location', LocationController::class);
