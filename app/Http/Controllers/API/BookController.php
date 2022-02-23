87befb0dedccfa9392e62ded3558429406573ba9
seeda06 <86487372+seeda06@users.noreply.github.com>
2022-02-24 00:40:40 +0700

    week13

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -26,6 +26,11 @@ class BookController extends Controller
     public function store(Request $request)
     {
         $requestData = $request->all();
+        if ($request->hasFile('image')) {
+            $filename = $request->file('image')->store('uploads', 'public');
+            $requestData['image'] = asset('storage/'.$filename);
+        }
+    
         $book = Book::create($requestData);
         return ["success" => true, "data" => $book];
     }
@@ -51,6 +56,11 @@ class BookController extends Controller
     public function update(Request $request, $id)
     {
         $requestData = $request->all();
+        if ($request->hasFile('image')) {
+            $filename = $request->file('image')->store('uploads', 'public');
+            $requestData['image'] = asset('storage/'.$filename);
+        }
+    
         $book = Book::findOrFail($id);
         $success = $book->update($requestData);
         return ["success" => $success];
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\API\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\LocationController;
-
+use App\Http\Controllers\API\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::apiResource('book', BookController::class);
 Route::apiResource('location', LocationController::class);
+Route::post('/sanctum/token', [UserController::class, 'token']);
+Route::post('/sanctum/token/register', [UserController::class, 'register']);
